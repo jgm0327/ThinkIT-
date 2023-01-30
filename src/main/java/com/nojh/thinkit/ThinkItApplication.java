3a7b2591e4db5910e820bbf9b6a19a95e7030784
jgm0327 <gmjang6349@gmail.com>
2023-01-30 17:21:22 +0900

    초기 설정

---
+++ src/main/java/com/nojh/thinkit/ThinkItApplication.java
@@ -0,0 +1,13 @@
+package com.nojh.thinkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nojh/thinkit/ThinkItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nojh.thinkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
