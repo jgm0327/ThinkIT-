39f9576e5ea67e0da5d6820ee677bb3451626def
jgm0327 <gmjang6349@gmail.com>
2023-02-01 18:12:35 +0900

    #2 랜덤으로 10문제
    각 과목에 해당하는 문제들을 가져와서 10문제이상을 보낸다.

--- src/main/java/com/nojh/thinkit/study/repository/ProblemRepository.java
+++ src/main/java/com/nojh/thinkit/study/repository/ProblemRepository.java
@@ -2,7 +2,11 @@ package com.nojh.thinkit.study.repository;
 
 import com.nojh.thinkit.study.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
-    
+    @Query(value = "select * from problem where subject_id = :id order by rand() limit :cnt", nativeQuery = true)
+    List<Problem> findBySubject_Id(int id, int cnt);
 }
