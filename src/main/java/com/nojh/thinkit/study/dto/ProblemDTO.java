d1e61656334979f2c0b193d20de0ffc3ab9698f5
jgm0327 <gmjang6349@gmail.com>
2023-02-01 18:11:23 +0900

    #2 Builder 추가
    ProblemDTO에 Builder를 추가해서 데이터 설정할 수 있게 함

--- src/main/java/com/nojh/thinkit/study/dto/ProblemDTO.java
+++ src/main/java/com/nojh/thinkit/study/dto/ProblemDTO.java
@@ -1,10 +1,16 @@
 package com.nojh.thinkit.study.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProblemDTO {
     private List<String> titles;
     private List<String> selects;
