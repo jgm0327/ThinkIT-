ef822a531888f1e0b463cf332c19174c6ad4ed5d
jgm0327 <gmjang6349@gmail.com>
2023-02-07 19:14:35 +0900

    #5 주석 추가

--- src/main/java/com/nojh/thinkit/auth/controller/AuthController.java
+++ src/main/java/com/nojh/thinkit/auth/controller/AuthController.java
@@ -24,13 +24,15 @@ public class AuthController {
     @PreAuthorize("permitAll")
     @PostMapping(value = "/join", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, String>> join(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult) {
+        // 회원가입 정보가 정해진 규칙을 따르지 않으면 fail과 error 내용 전달
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(Map.of("result", "fail"));
         }
+        // 이미 존재하는 username을 쓰면 존재한다는 exist 값 전달
         if (joinService.join(userDTO) == -1) {
             return ResponseEntity.badRequest().body(Map.of("result", "exist"));
         }
-        log.info(userDTO);
+        // 회원가입이 정상적으로
         return ResponseEntity.ok(Map.of("result", "success"));
     }
 
